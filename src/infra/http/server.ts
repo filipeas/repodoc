b98181bb8b818194419fe9426a4f9acb125ba1ab
filipeas <felipealvessampaio@hotmail.com>
2022-03-29 21:37:57 -0300

    ajustes de configuracao do projeto #2

---
+++ src/infra/http/server.ts
@@ -0,0 +1,25 @@
+import 'dotenv/config';
+import 'reflect-metadata';
+import 'express-async-errors';
+
+import express from 'express';
+
+// import {handleException} from '';
+// import '../container';
+
+import createConnection from '../typeorm';
+
+import { routes } from './routes';
+
+createConnection();
+const app = express();
+
+app.use(express.json());
+
+app.use(routes);
+
+// app.use(handleException);
+
+app.listen(process.env.APP_PORT, () => {
+    console.log(`Server rodando em ${process.env.APP_URL}`);
+});
