a0eb596b7eaf54843c7ff79ac4341141d7ec800a
filipeas <felipealvessampaio@hotmail.com>
2022-03-31 13:06:11 -0300

    identando arquivo index #3

--- src/infra/http/routes/index.ts
+++ src/infra/http/routes/index.ts
@@ -5,11 +5,13 @@ import { usersRouter } from './users.routes';
 const routes = Router();
 
 routes.get('/', (request, response) => {
-    return response.json(
-        {
-            "author": "Filipe A.S",
-            "message": "Bem vindo a API do RepoDoc! Por favor, consulte nosso repositório para mais informações: https://github.com/filipeas/repodoc"
-        }).send();
+    return response
+        .json({
+            author: 'Filipe A.S',
+            message:
+                'Bem vindo a API do RepoDoc! Por favor, consulte nosso repositório para mais informações: https://github.com/filipeas/repodoc',
+        })
+        .send();
 });
 
 routes.use('/auth', authenticateRouter);
