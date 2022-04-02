4b849393326d9b0649af6fb76a79bb4f5012ec78
filipeas <felipealvessampaio@hotmail.com>
2022-04-02 13:57:05 -0300

    iniciando caso de uso de listar documento pelo id do usuario #8

---
+++ src/domain/documents/useCases/list-documents-by-user-id/ListDocumentByUserIDUseCase.spec.ts
@@ -0,0 +1,5 @@
+describe('Teste de listar documento pelo ID do usuário', () => {
+    it('listar documento pelo ID do usuário', async () => {});
+    it('listar documento pelo ID sem informar o ID do usuário', async () => {});
+    it('listar documento pelo ID do usuário com ID inexistente', async () => {});
+});
